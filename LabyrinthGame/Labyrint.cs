b9806703b9e5e2b3ca1f93503b3bd833f03b5d6e
Magnus Tidqvist <magnus.tidqvist@outlook.com>
2018-12-12 11:31:37 +0100

    Skal av GameEnging och Labyrint klassen

---
+++ LabyrinthGame/Labyrint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabyrinthGame
+{
+    enum SquareStatus {wall, free};
+    class Labyrint
+    {
+        public SquareStatus[,] Grid { get; set; }
+
+        public Labyrint()
+        {
+            Grid = new SquareStatus[3, 3] {
+                { SquareStatus.free, SquareStatus.free, SquareStatus.free },
+                { SquareStatus.free, SquareStatus.wall, SquareStatus.free },
+                { SquareStatus.free, SquareStatus.free, SquareStatus.free } };
+        }
+    }
+}
--- LabyrinthGame/Program.cs
+++ LabyrinthGame/Program.cs
@@ -7,6 +7,14 @@ namespace LabyrinthGame
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Fellow Developers!");
+            Console.WriteLine("Rädda Joppe, DÖD ELLER LEVANDE!");
+            Console.WriteLine("Tjeeena");
+            var message = "Kul att se er!";
+            Console.WriteLine();
+           
+
         }
+
+        
     }
 }
